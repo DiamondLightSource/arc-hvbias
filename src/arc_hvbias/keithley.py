0c703b9351f353dbda344d8dc08365b55d913731
Ollie Copping <oliver.copping@diamond.ac.uk>
2023-11-28 11:30:44 +0000

    Fixed Literal type import

--- src/arc_hvbias/keithley.py
+++ src/arc_hvbias/keithley.py
@@ -6,9 +6,10 @@ import codecs
 import math
 import warnings
 from datetime import datetime
-from typing import List, Literal, Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 import cothread
+from typing_extensions import Literal
 
 from .comms import Comms
 
